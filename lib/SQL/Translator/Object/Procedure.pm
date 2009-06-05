76764964788aaca6b6e9a3888d4c424e81de47fd
Justin Hunter <justin.d.hunter@gmail.com>
2009-06-05 15:50:34 -0700

    updates

--- lib/SQL/Translator/Object/Procedure.pm
+++ lib/SQL/Translator/Object/Procedure.pm
@@ -2,8 +2,13 @@ package SQL::Translator::Object::Procedure;
 use Moose;
 use MooseX::Types::Moose qw(Str);
 use SQL::Translator::Types qw();
+use SQL::Translator::Object::Schema;
 
 has 'name' => (is => 'ro', isa => Str, required => 1);
 has 'contents' => (is => 'ro', isa => Str, required => 1);
+has 'parameters' => (is => 'ro', isa => Maybe[ArrayRef[Int|Str]], required => 0);	
+has 'owner' => (is => 'ro', isa => Str, required => 1);
+has 'comments' => (is => 'ro', isa => Str, required => 0);
+has 'schema' => (is => 'ro', isa => Schema, required => 1, default => sub { SQL::Translator::Object::Schema->new });
 
 1;
