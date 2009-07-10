d7cecc500cb91743be9d3175f220646ff1debbb3
Justin Hunter <justin.d.hunter@gmail.com>
2009-07-09 22:59:25 -0700

    use Sub::Exporter instead of Exporter

--- lib/SQL/Translator/Constants.pm
+++ lib/SQL/Translator/Constants.pm
@@ -1,20 +1,10 @@
 package SQL::Translator::Constants;
 use DBI qw(:sql_types);
-use Exporter ();
-
-BEGIN {
-    @ISA = qw(Exporter);
-
-    @EXPORT    = ();
-    @EXPORT_OK = ();
-    %EXPORT_TAGS = (
-        sqlt_types => [
-            qw(),
-            @{$DBI::EXPORT_TAGS{sql_types}}
-        ]
-    );
-
-    Exporter::export_ok_tags(keys %EXPORT_TAGS);
-}
+use Sub::Exporter -setup => {
+    exports => [ @{$DBI::EXPORT_TAGS{sql_types}} ],
+    groups => {
+        sqlt_types => [ @{$DBI::EXPORT_TAGS{sql_types}} ] 
+    }
+};
 
 1;
