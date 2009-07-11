03749994bfb02e8476c505357047be99b0710981
Justin Hunter <justin.d.hunter@gmail.com>
2009-07-11 12:28:52 -0700

    add foreign key action constants

--- lib/SQL/Translator/Constants.pm
+++ lib/SQL/Translator/Constants.pm
@@ -3,8 +3,15 @@ use DBI qw(:sql_types);
 use Sub::Exporter -setup => {
     exports => [ @{$DBI::EXPORT_TAGS{sql_types}} ],
     groups => {
-        sqlt_types => [ @{$DBI::EXPORT_TAGS{sql_types}} ] 
+        sqlt_types => [ @{$DBI::EXPORT_TAGS{sql_types}} ],
+        sqlt_fk_actions => [ qw(SQLT_FK_CASCADE SQLT_FK_RESTRICT SQLT_FK_SET_NULL SQLT_FK_NO_ACTION SQLT_FK_SET_DEFAULT) ],
     }
 };
+
+use constant SQLT_FK_CASCADE     => 0;
+use constant SQLT_FK_RESTRICT    => 1;
+use constant SQLT_FK_SET_NULL    => 2;
+use constant SQLT_FK_NO_ACTION   => 3;
+use constant SQLT_FK_SET_DEFAULT => 4;
 
 1;
