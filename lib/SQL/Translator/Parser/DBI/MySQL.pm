6fbb2193ef6a764859b0c229b98c614b01cbf12d
Justin Hunter <justin.d.hunter@gmail.com>
2009-08-05 15:53:28 -0700

    initial mysql dbi parser role

--- lib/SQL/Translator/Parser/DBI/MySQL.pm
+++ lib/SQL/Translator/Parser/DBI/MySQL.pm
@@ -1,3 +1,22 @@
 use MooseX::Declare;
 role SQL::Translator::Parser::DBI::MySQL {
+    use MooseX::Types::Moose qw(HashRef Str);
+    use SQL::Translator::Types qw(View);
+
+    method _get_view_sql(View $view) {
+        #my ($sql) = $self->dbh->selectrow_array('');
+        #return $sql;
+        return '';
+    }
+
+    method _is_auto_increment(HashRef $column_info) {
+        return $column_info->{mysql_is_auto_increment};
+    }
+
+    method _column_default_value(HashRef $column_info) {
+        my $default_value = $column_info->{COLUMN_DEF};
+        $default_value =~ s/::.*$// if defined $default_value
+
+        return $default_value;
+    }
 }
