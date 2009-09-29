98cff1fd9462e65817abcc3952b0ae848ada572c
Justin Hunter <justin.d.hunter@gmail.com>
2009-09-29 16:49:34 -0700

    remove auto-deref and use the Native::Trait handles

--- lib/SQL/Translator/Object.pm
+++ lib/SQL/Translator/Object.pm
@@ -6,26 +6,24 @@ class SQL::Translator::Object {
 
     has '_comments' => (
         traits => ['Array'],
-        is => 'rw',
         isa => ArrayRef,
         handles => {
+            _comments           => 'elements',
             add_comment         => 'push',
             remove_last_comment => 'pop',
         },
         default => sub { [] },
-        auto_deref => 1,
     );
 
     has '_options' => (
         traits => ['Array'],
-        is => 'rw',
         isa => ArrayRef,
         handles => {
+            _options           => 'elements',
             add_option         => 'push',
             remove_last_option => 'pop',
         },
         default => sub { [] },
-        auto_deref => 1,
     );
 
     has '_extra' => (
@@ -40,7 +38,6 @@ class SQL::Translator::Object {
             add_extra    => 'set',
         },
         default => sub { {} },
-        auto_deref => 1,
     );
 
     multi method comments(Str $comment) { $self->add_comment($comment); $self->comments }
