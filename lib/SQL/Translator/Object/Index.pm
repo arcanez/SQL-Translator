52137c8d1aee7154786de27d89508c69e6f3b575
Justin Hunter <justin.d.hunter@gmail.com>
2009-09-29 17:26:06 -0700

    allow setting of fields (columns) like ->fields('foo,bar') or ->fields([ qw/foo bar/ ])

--- lib/SQL/Translator/Object/Index.pm
+++ lib/SQL/Translator/Object/Index.pm
@@ -1,6 +1,7 @@
 use MooseX::Declare;
 class SQL::Translator::Object::Index extends SQL::Translator::Object {
     use MooseX::Types::Moose qw(HashRef Str);
+    use MooseX::MultiMethods;
     use SQL::Translator::Types qw(Column Table);
 
     has 'table' => (
@@ -29,7 +30,6 @@ class SQL::Translator::Object::Index extends SQL::Translator::Object {
 
             ## compat
             get_fields    => 'values',
-            fields        => 'keys',
         },
         default => sub { my %hash = (); tie %hash, 'Tie::IxHash'; return \%hash },
     );
@@ -42,6 +42,20 @@ class SQL::Translator::Object::Index extends SQL::Translator::Object {
     );
 
     around add_column(Column $column) { $self->$orig($column->name, $column) }
+
+    multi method fields(Str $columns) {
+        for (split /,/, $columns) { die "Column $_ does not exist!" unless $self->table->exists_column($_) }
+        $self->add_column($self->table->get_column($_)) for split /,/, $columns;
+        $self->column_ids;
+    }
+
+    multi method fields(@columns) {
+        for (@columns) { die "Column $_ does not exist!" unless $self->table->exists_column($_) }
+        $self->add_column($self->table->get_column($_)) for @columns;
+        $self->column_ids;
+    }
+
+    multi method fields(Any $) { $self->column_ids }
 
     method is_valid { $self->table && scalar $self->get_columns ? 1 : undef }
 }
