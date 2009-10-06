f3330f9a32502405e4b05c3c73f66d9f86452d8f
Justin Hunter <justin.d.hunter@gmail.com>
2009-10-06 16:27:24 -0700

    View extends Table

--- lib/SQL/Translator/Object/View.pm
+++ lib/SQL/Translator/Object/View.pm
@@ -1,33 +1,16 @@
 use MooseX::Declare;
-class SQL::Translator::Object::View extends SQL::Translator::Object {
+class SQL::Translator::Object::View extends SQL::Translator::Object::Table {
     use MooseX::Types::Moose qw(HashRef Str);
-    use SQL::Translator::Types qw(Column);
-    
-    has 'name' => (
-        is => 'rw',
-        isa => Str,
-        required => 1
-    );
-    
-    has 'columns' => (
-        traits => ['Hash'],
-        is => 'rw',
-        isa => HashRef[Column],
-        handles => {
-            exists_column => 'exists',
-            column_ids    => 'keys',
-            get_columns   => 'values',
-            get_column    => 'get',
-            add_column    => 'set',
-        },
-        default => sub { my %hash = (); tie %hash, 'Tie::IxHash'; return \%hash },
-    );
+    use SQL::Translator::Types qw(Column Schema);
     
     has 'sql' => (
         is => 'rw',
         isa => Str,
-        required => 1
     );
 
-    around add_column(Column $column) { $self->$orig($column->name, $column) }
+    around add_column(Column $column does coerce) {
+        die "Can't use column name " . $column->name if $self->exists_column($column->name) || $column->name eq '';
+        $column->table($self);
+        return $self->$orig($column->name, $column);
+    }
 }
