25aa222bad5e3ec57aec59eb0ac957d2483462c8
Justin Hunter <justin.d.hunter@gmail.com>
2010-02-10 09:49:34 -0800

    yes, we want column comments for SQLite

--- lib/SQL/Translator/Parser/DDL/SQLite.pm
+++ lib/SQL/Translator/Parser/DDL/SQLite.pm
@@ -56,6 +56,7 @@ role SQL::Translator::Parser::DDL::SQLite {
                     default_value     => $fdata->{'default'},
                     is_auto_increment => $fdata->{'is_auto_inc'},
                     is_nullable       => $fdata->{'is_nullable'},
+                    comments          => $fdata->{'comments'},
                     table             => $table,
                 });
                 $table->add_column($field);
