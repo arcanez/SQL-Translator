f68cdb779367fecbdd7853f11e7bb8c83d40fe87
Justin Hunter <justin.d.hunter@gmail.com>
2010-09-22 08:45:18 -0700

    trim down imports

--- lib/SQL/Translator/Object/View.pm
+++ lib/SQL/Translator/Object/View.pm
@@ -1,7 +1,6 @@
 use MooseX::Declare;
 class SQL::Translator::Object::View extends SQL::Translator::Object::Table {
-    use MooseX::Types::Moose qw(HashRef Str);
-    use SQL::Translator::Types qw(Column Schema);
+    use MooseX::Types::Moose qw(Str);
     
     has 'sql' => (
         is => 'rw',
